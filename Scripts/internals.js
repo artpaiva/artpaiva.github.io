0773bae3a9ea4940dd17b6521f8b7c45ff5d7eae
arturmntr <arturmntr@github.com>
2019-09-17 00:03:10 -0300

    [Fix] Top Bar change on Scroll

--- Scripts/internals.js
+++ Scripts/internals.js
@@ -1,10 +1,8 @@
-"use strict";
 var scrolled = false;
 window.onscroll = function (){ 
-	"use strict";
-	console.log("scroll");
+	console.log(document.documentElement.scrollTop);
 	var bar  = document.getElementById('barra');
-	if (document.body.scrollTop > 20 ) {
+	if (document.documentElement.scrollTop > 20 ) {
 		bar.classList.add("scrolled");
 	} 
 	else {
@@ -16,12 +14,12 @@ function openInfo(caller){
 	document.getElementById('marqueeInfo').classList.toggle('active');
 }
 function toTop(){
-	document.body.scrollTo(0, 0);
+	document.documentElement.scrollTo(0, 0);
 }
 
 window.onload = function () {
 	var tips = document.getElementsByClassName('board-tip');
 	for(var i=0; i < tips.length; i++){
-		tips[i].style.marginLeft = -1 * tips[i].clientWidth/2;
+		tips[i].style.marginLeft = (tips[i].clientWidth/2 * -1) + "px";
 	}
 }
